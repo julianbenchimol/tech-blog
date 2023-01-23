7a7996d3f0a47a6b4e99093439ac906b4e7de66b
JulianBenchimol <jbenchimol1999@gmail.com>
2023-01-23 18:09:27 -0500

    fixed withauth issues

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,24 +48,25 @@ router.get('/post/:id', async (req, res) =>{
     }
 })
 
-// router.get('/profile', withAuth, async (req, res) => {
-//     try {
-//       // Find the logged in user based on the session ID
-//       const userData = await User.findByPk(req.session.user_id, {
-//         attributes: { exclude: ['password'] },
-//         include: [{ model: Project }],
-//       });
-  
-//       const user = userData.get({ plain: true });
-  
-//       res.render('profile', {
-//         ...user,
-//         logged_in: true
-//       });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+router.get('/profile', withAuth, async (req, res) => {
+    try{
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: {exclude: ['password']},
+            include: [{model: Posts}]
+        })
+
+        const user = userData.get({plain:true});
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    }
+    catch(err){
+
+    }
+
+  });
 
 router.get('/login', (req, res) =>{
     if(req.session.logged_in){
