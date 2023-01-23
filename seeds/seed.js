9f10e425822b9af70e0f4f55fc8005008ed5983d
JulianBenchimol <jbenchimol1999@gmail.com>
2023-01-23 16:54:35 -0500

    fixed seeding database bug

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const sequelize = require('../config/connection');
+const { User, Posts} = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async()=>{
+    await sequelize.sync({force: true});
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for(const post of postData){
+        await Posts.create({
+            ...post,
+            user_id: users[Math.floor(Math.random() * users.length)].id
+        })
+    }
+    process.exit(0)
+}
+
+seedDatabase();
