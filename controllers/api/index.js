a66e18028a2282eaa7d48d3c9b17ae03e17fb0c9
JulianBenchimol <jbenchimol1999@gmail.com>
2023-01-23 15:42:57 -0500

    fixed server route issues

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+
+router.use('/posts', postRoutes)
+router.use('/users', userRoutes)
+
+module.exports = router;
