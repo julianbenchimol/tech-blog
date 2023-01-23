0cdd00332b32dfb78be6e0ee82061ab9efefdf99
JulianBenchimol <jbenchimol1999@gmail.com>
2023-01-23 17:54:14 -0500

    finished up fixing project

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,24 +48,24 @@ router.get('/post/:id', async (req, res) =>{
     }
 })
 
-router.get('/profile', withAuth, async (req, res) =>{
-    try{
-        const userData = await User.findByPk(req.session.user_id, {
-            attributes: {exclude: ['password']},
-            include: [{model: Posts}],
-        })
-
-        const user = userData.get({plain: true});
-
-        res.render('profile', {
-            ...user,
-            logged_in: true
-        })
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+// router.get('/profile', withAuth, async (req, res) => {
+//     try {
+//       // Find the logged in user based on the session ID
+//       const userData = await User.findByPk(req.session.user_id, {
+//         attributes: { exclude: ['password'] },
+//         include: [{ model: Project }],
+//       });
+  
+//       const user = userData.get({ plain: true });
+  
+//       res.render('profile', {
+//         ...user,
+//         logged_in: true
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
 router.get('/login', (req, res) =>{
     if(req.session.logged_in){
@@ -74,3 +74,5 @@ router.get('/login', (req, res) =>{
     }
     res.render('login')
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
@@ -8,9 +10,16 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create();
+
 const sess = {
   secret: 'Super duper secret',
-  cookie: {},
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -20,8 +29,12 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
